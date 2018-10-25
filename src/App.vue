5508d9622c7f1ab8b323f6e9a2777662e5125fa9
tzmanics <tzmanics@gmail.com>
2018-10-25 15:52:12 -0400

    add vue-cli files + initial component changes

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Tosh-my-Gosh!</span>
+    </header>
+    <main>
+      <Chat />
+    </main>
+  </div>
+</template>
+
+<script>
+import Chat from './components/Chat'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="chat">
+    <div class="chat-area">
+      <h3>Chat Time</h3> 
+      <hr>
+    </div>
+    <div class="chat-body">
+      <div class="messages">
+      </div>
+    </div>
+    <div class="chat-footer">
+      <form @submit.prevent="sendMessage">
+	<div class="form-input">
+	  <label for="user">User:</label>
+	  <input type="text" v-model="user" class="form-control">
+	  <label for="message">Message:</label>
+	  <input type="text" v-model="message" class="form-control">
+	</div>
+	<button type="submit" class="button">Send</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chat',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
