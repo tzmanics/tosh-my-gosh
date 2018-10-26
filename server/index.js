78495d380813922a62c37e7b9576170d9b380f00
tzmanics <tzmanics@gmail.com>
2018-10-25 21:53:10 -0400

    add socketio msg work

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const app = express()
 
 const port = process.env.PORT || 3001
 
-const server = app.listen(port, function() {
-	console.log(`Example app listening on port ${port}!`))
-}
+const server = app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`)
+})
 
 const io = require('socket.io')(server)
 
-io.on('connection', () => console.log(socket.id))
+io.on('connection', (socket) => {
+  console.log(socket.id)
+  socket.on('SEND_MESSAGE', (data) => io.emit('MESSAGE', data))
+})
 
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -5,16 +5,17 @@
       <hr>
     </div>
     <div class="chat-body">
-      <div class="messages">
+      <div class="messages" v-for="(msg, index) in messages" :key="index">
+        <p>{{ msg.user }}: {{ msg.message }}</p>
       </div>
     </div>
     <div class="chat-footer">
       <form @submit.prevent="sendMessage">
 	<div class="form-input">
-	  <label for="user">User:</label>
-	  <input type="text" v-model="user" class="form-control">
-	  <label for="message">Message:</label>
-	  <input type="text" v-model="message" class="form-control">
+		<label for="user">User:</label>
+		<input type="text" v-model="user" class="form-control">
+		<label for="message">Message:</label>
+		<input type="text" v-model="message" class="form-control">
 	</div>
 	<button type="submit" class="button">Send</button>
       </form>
@@ -23,11 +24,31 @@
 </template>
 
 <script>
+import io from 'socket.io-client'
 export default {
-  name: 'chat',
   data () {
     return {
+      user: '',
+      message: '',
+      messages: [],
+      socket: io('localhost:3001')
     }
+  },
+  methods: {
+    sendMessage (e) {
+      e.preventDefault()
+
+      this.socket.emit('SEND_MESSAGE', {
+        user: this.user,
+        message: this.message
+      })
+      this.message = ''
+    }
+  },
+  mounted () {
+    this.socket.on('MESSAGE', (data) => {
+      this.messages = [...this.messages, data]
+    })
   }
 }
 </script>
