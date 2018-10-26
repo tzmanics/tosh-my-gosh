ae056a9440a7ee89b047eec279fdd023bab894d7
tzmanics <tzmanics@gmail.com>
2018-10-26 00:42:27 -0400

    add pick random toshoji

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="chat">
     <div class="chat-area">
-      <h3>Chat Time</h3> 
+      <h3>Stop, Toshi Time!</h3> 
       <hr>
     </div>
     <div class="chat-body">
       <div class="messages" v-for="(msg, index) in messages" :key="index">
-        <p>{{ msg.user }}: {{ msg.message }}</p>
+        <p>user: {{msg.user}}</p>
+        <img :src="msg.message" alt="toshoji">
       </div>
     </div>
     <div class="chat-footer">
       <form @submit.prevent="sendMessage">
-	<div class="form-input">
-		<label for="user">User:</label>
-		<input type="text" v-model="user" class="form-control">
-		<label for="message">Message:</label>
-		<input type="text" v-model="message" class="form-control">
-	</div>
-	<button type="submit" class="button">Send</button>
+      	<div class="form-input">
+      		<label for="user">User:</label>
+      		<input type="text" v-model="user" class="form-control">
+      	</div>
+      	<button type="submit" class="button">Send Random Toshoji</button>
       </form>
     </div>
   </div>
@@ -31,16 +30,33 @@ export default {
       user: '',
       message: '',
       messages: [],
-      socket: io('localhost:3001')
+      socket: io('localhost:3001'),
+      toshiGallery: [
+        'https://bit.ly/2CFJPD0',
+        'https://bit.ly/2ENbN2u',
+        'https://bit.ly/2Rh2toD',
+        'https://bit.ly/2OR9PT4',
+        'https://bit.ly/2CFsQkdi',
+        'https://bit.ly/2qbmgun',
+        'https://bit.ly/2O8aLgC',
+        'https://bit.ly/2z1YwNp',
+        'https://bit.ly/2CHUH35',
+        'https://bit.ly/2JigmAn',
+        'https://bit.ly/2OQxUcH',
+        'https://bit.ly/2PSSSUV',
+        'https://tinyurl.com/yam6wx7e',
+        'https://tinyurl.com/yb8jmdms',
+        'https://tinyurl.com/ya5ag5zr',
+        'https://tinyurl.com/ybu5fhdw'
+      ]
     }
   },
   methods: {
     sendMessage (e) {
       e.preventDefault()
-
       this.socket.emit('SEND_MESSAGE', {
         user: this.user,
-        message: this.message
+        message: this.toshiGallery[Math.floor(Math.random() * 16) + 1]
       })
       this.message = ''
     }
@@ -55,4 +71,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+  img {
+    max-width: 200px;
+  }
 </style>
